cc8a9c6600b614e65d464c7dfbd8161ad21c581e
Miguel Rios <miguel@miguelrios.org>
2013-06-01 19:57:14 -0700

    added index page and consolidated tiles

--- js/prmap.js
+++ js/prmap.js
@@ -1,83 +1,120 @@
 var PRMap = klass(function (options) {
   this.options = options;
-  this.options.tiles = Array.isArray(options.tiles) ? options.tiles : [options.tiles];
+  this.tiles = Array.isArray(options.tiles) ? options.tiles : [options.tiles || 'isla'];
+  this.main_tile = this.tiles[0];
   this.options.events = options.events || {};
-  this.paths = {};
-  this.paths['barrios'] = "../geotiles/barrios-geo.json";
-  this.paths['pueblos'] = "../geotiles/pueblos-geo.json";
+  this.path = "../geotiles/puertorico-geo.json";
+  this.maps = {};
+  this.meta_attributes = {
+    "barrios": {
+      width: 1,
+      id: "GEO_ID"
+    },
+    "pueblos": {
+      width: 2,
+      id: "COUNTY"
+    },
+    "isla":{
+      width: 3,
+      id: "GEOID"
+    }
+  };
+  this.size_attributes = {
+    "small": {
+      width: 480,
+      height: 160,
+      scale: 23000
+    },
+    "medium": {
+      width: 960,
+      height: 320,
+      scale: 23000
+    },
+    "large": {
+      width: 1200,
+      height: 400,
+      scale: 23000
+    }
+  };
+  this.width = this.size_attributes[this.options.size || "medium"].width;
+  this.height = this.width/3;
+  this.scale = this.width*27;
   this.draw();
 })
 .methods({
 	draw: function () {
     var self = this;
-    var width = 960,
-      height = 300;
+    
     var color_scale = d3.scale.category20b();
     var svg = d3.select(this.options.node).append("svg")
-      .attr("width", width)
-      .attr("height", height);
+      .attr("width", this.width)
+      .attr("height", this.height);
 
     var projection = d3.geo.mercator()
-      .scale(125000)
-      .center([-66.401367,18.23033])
-      .translate([width / 2, height / 2]);
+      .scale(this.scale)
+      .center([-66.251367,18.20033])
+      .translate([this.width/2,this.height/2])
+      // .translate([width / 2, height / 2]);
 
-    var path = d3.geo.path()
-      .projection(projection);
+    d3.json(self.path, function(error, data) {
+      // zoom to island
+      var path = d3.geo.path()
+        .projection(projection);
 
-    this.options.tiles.forEach(function(tile){
-      d3.json(self.paths[tile], function(error, data) {
-        // var barrios_path = topojson.feature(barrios, barrios.objects.barrios);
-      self.top_map = svg.selectAll("." + tile)
-        .data(data.features)
-        .enter()
-          .append("path")  
-            .attr("d", path)
-            .attr("class", tile)
-            .style("stroke-width", tile == "barrios" ? 1 : 2)
-            // .style("fill", function(d){return color_scale(Math.floor(Math.random()*20))})
-            .style("fill", "rgba(255,255,255,0)")
-            .style("stroke", "#333")
-            .on("click",self.options.events.on_click)
-            .on("mouseover", function(d){
-              d3.select(this).style("stroke-width", 3);
-              self.options.events.on_mouseover && self.options.events.on_mouseover(d,this);
-            })
-            .on("mouseout", function(d){
-              d3.select(this).style("stroke-width", tile == "barrios" ? 1 : 2);
-              self.options.events.on_mouseout && self.options.events.on_mouseout(d,this);
-            });
-      self.tile_attribute = tile == 'barrios' ? "GEO_ID" : "COUNTY";
-      if(tile == self.options.tiles[self.options.tiles.length -1]){
-        self.options.on_ready();
+      self.tiles.forEach(function(tile){
+        var width = self.meta_attributes[tile].width;
+        var tiletype = self.meta_attributes[tile].id;
+        self.maps[tile] = svg.selectAll("." + tile)
+          .data(data[tile].features)
+          .enter()
+            .append("path")  
+              .attr("d", path)
+              .attr("class", tile)
+              .attr("data-tiletype",tiletype)
+              .attr("data-name",function(d){return d.properties.NAME})
+              .style("stroke-width", width)
+              .style("fill", "rgba(255,255,255,0)")
+              .style("stroke", "#333")
+              .on("click",self.options.events.on_click)
+              .on("mouseover", function(d){
+                d3.select(this).style("stroke-width", 3);
+                self.options.events.on_mouseover && self.options.events.on_mouseover(d,this);
+              })
+              .on("mouseout", function(d){
+                d3.select(this).style("stroke-width", width);
+                self.options.events.on_mouseout && self.options.events.on_mouseout(d,this);
+              });
+      });
+      if(self.options.on_ready){
+        self.options.on_ready(self.maps);
       }
     });
-  });
-},
-encode_qual: function(datamap, colormap){
+  },
+  encode_qual: function(datamap, colormap){
+    this.maps[this.main_tile]
+      .style("fill", function(d){
+        var id = d.properties[d3.select(this).attr("data-tiletype")];
+        return colormap[datamap[id]];
+      });
+  },
+  encode_quan: function(datamap){
+    var min, max;
+    var a = [];
+    var self = this;
+    Object.keys(datamap).forEach(function(k){
+      a.push(+datamap[k]);
+    });
+    var domain = [d3.min(a), d3.max(a)];
+    
+    var quantize = d3.scale.quantize()
+        .domain(domain)
+        .range(d3.range(9).map(function(i) { return "q" + i + "-9"; }));
 
-  this.top_map
-    .style("fill", function(d){
-      return colormap[datamap[d.properties.COUNTY || d.properties.GEO_ID]];
-    });
-},
-encode_quan: function(datamap){
-  var min, max;
-  var a = [];
-  var self = this;
-  Object.keys(datamap).forEach(function(k){
-    a.push(+datamap[k]);
-  });
-  var domain = [d3.min(a), d3.max(a)];
-  console.log(domain)
-  var quantize = d3.scale.quantize()
-      .domain(domain)
-      .range(d3.range(9).map(function(i) { return "q" + i + "-9"; }));
-  this.top_map
-    .style("fill", undefined)
-    .attr("class", function(d){
-      console.log(self.tile_attribute)
-      return quantize(+datamap[d.properties[self.tile_attribute]]) || "grey";
-    });
-}
+    this.maps[this.main_tile]
+      .style("fill", undefined)
+      .attr("class", function(d){
+        var id = d.properties[d3.select(this).attr("data-tiletype")];
+        return quantize(+datamap[id]) || "grey";
+      });
+  }
 })
